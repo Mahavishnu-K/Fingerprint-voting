e0035febce172eb535c652b231edc642f62d8e22
Mahavishnu-K <mahavishnu005sk@gmail.com>
2025-04-30 17:10:22 +0530

    backend fix

--- frontend/appwriteConfig.js
+++ frontend/appwriteConfig.js
@@ -11,7 +11,7 @@ const databases = new Databases(client);
 const storage = new Storage(client);
 
 const DATABASE_ID = '68107e44000a5ab2cc85';
-const COLLECTION_ID = '68107e4e0033792df863';
+const COLLECTION_ID = '681209580029497a3c02';
 const STORAGE_BUCKET_ID = '68107e5f001578ed6cc2';
 
 export { client, account, databases, storage, ID, DATABASE_ID, COLLECTION_ID, STORAGE_BUCKET_ID, Query };
